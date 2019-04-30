05f12af0cf8e93e8a75e09b880c59ff466be169c
Tuancm <duytuancm9@gmail.com>
2019-04-30 14:47:54 +0700

    update connect sql

--- php/content/connect.php
+++ php/content/connect.php
@@ -2,7 +2,7 @@
 $server_username = "root";
 $server_password = "";
 $server_host = "localhost";
-$database = "bus_v1.0";
+$database = "webmap";
 $conn = mysqli_connect($server_host,$server_username,$server_password,$database) or die("<script> alert('Không thể kết nối CSDL!')</script>");
 mysqli_query($conn,"SET NAMES 'UTF8'");
 ?>
